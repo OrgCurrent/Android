68ef1c7180e9139b0e5c704f4b8f5c70a097cf73
shall <hall.steven.g@gmail.com>
2014-05-25 22:16:35 -0700

    re-org templates

--- server/email_test.js
+++ server/email_test.js
@@ -1,5 +1,14 @@
-var mailer = require("nodemailer"),
-    config = require("./server_config/settings");
+var fs         = require("fs"),
+    mailer     = require("nodemailer"), 
+    config     = require("./server_config/settings"),
+    Handlebars = require("handlebars");
+
+var template, html;
+var templatePath = __dirname + '/mail-templates/responsive/base_boxed_basic_query.html';
+var source = fs.readFile(templatePath, 'utf8', function (err, file) {
+    template = file;
+    console.log("template", template);
+});
 
 var smtpTransport = mailer.createTransport("SMTP",{
     service: "Gmail",
@@ -13,8 +22,7 @@ var mailOptions = {
     from: config.from,
     to: config.test,
     subject: "Please Verify Your Email Address",
-    text: "Hello world",
-    html: "<b>Hello world</b>"
+    html: template
 }
 
 smtpTransport.sendMail(mailOptions, function(error, response){
