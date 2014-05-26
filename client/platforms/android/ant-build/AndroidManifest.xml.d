b9ffcdb777bf2011d22d6381d30b9ab09ba1825d
keithGrout <keiwi.g@gmail.com>
2014-05-26 16:56:56 -0700

    add client packages

---
+++ client/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/user/Desktop/happyMeter/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/user/Desktop/happyMeter/platforms/android/AndroidManifest.xml \
---
+++ client/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.console": "0.2.8",
+    "org.apache.cordova.device": "0.2.9"
+}
+// BOTTOM OF METADATA
+});
