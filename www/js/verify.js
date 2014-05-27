43597f8fac9bfb0aa73ac324a9c9d3b30a87cb7a
Andrew Jones <acjones617@gmail.com>
2014-05-27 16:20:17 -0700

    Adds other two controllers as placeholders

--- www/js/verify.js
+++ www/js/verify.js
@@ -3,3 +3,9 @@ angular.module('app.verify', [])
 .controller('VerifyCtrl', function($scope) {
   console.log('verify');
 })
+.controller('EmailCtrl', function($scope) {
+  console.log('email');
+})
+.controller('OpinionCtrl', function($scope) {
+  console.log('opinion');
+})
