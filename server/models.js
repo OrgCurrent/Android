9c08eea55a2458eca7053fd49a1709cca91adcbc
shall <hall.steven.g@gmail.com>
2014-05-28 14:06:26 -0700

    formatting

--- server/models.js
+++ server/models.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost');
 
