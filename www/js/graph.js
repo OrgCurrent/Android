76d3cb4c8ec8190847f400418004b0abe25f2e2b
Andrew Jones <acjones617@gmail.com>
2014-05-28 16:22:39 -0700

    Creates simple d3 directive for selector plus ripples

--- www/js/graph.js
+++ www/js/graph.js
@@ -1,87 +1,91 @@
 angular.module('graph', [])
 
-.directive('helloWorld', function() {
+.directive('happyGraph', function() {
   var link = function(scope, element, attr) {
     console.log('hello world');
-    var initR = 100;
+    var initR = 10;
     var r = 400;
-    thickness = 20;
+    var thickness = 10;
 
-  var svg = d3.select(element[0])
-        .append("svg");
+    var click = function() {
+      d3.event.preventDefault();
+      svg.selectAll("g").remove();
 
-  svg.on("mousedown", click)
-    .on("mouseup", click);
+      var arc = d3.svg.arc()
+        .outerRadius(initR)
+        .innerRadius(initR - thickness);
+              
+      console.log(d3.mouse(svg.node()));
+      scope.$apply(function() {
+        scope.selectedPoint = d3.mouse(svg.node());
+      });
+              
+      var g = svg.selectAll("g.click")
+        .data([d3.mouse(svg.node())]);
 
-  function click() {
-    d3.event.preventDefault();
-
-    var arc = d3.svg.arc()
-            .outerRadius(initR)
-            .innerRadius(initR - thickness);
-            
-            console.log(d3.click(svg.node()));
-            
-    var g = svg.selectAll("g.click")
-            .data(d3.click(svg.node()), function (d) {
-                return d.identifier;
-            });
-
-    g.enter()
+      g.enter()
         .append("g")
         .attr("class", "click")
         .attr("transform", function (d) {
-            return "translate(" + d[0] + "," + d[1] + ")";
+          return "translate(" + d[0] + "," + d[1] + ")";
         })
         .append("path")
-            .attr("class", "arc")
-            .transition().duration(2000).ease('linear')
-            .attrTween("d", function (d) {
-                var interpolate = d3.interpolate(
-                        {startAngle: 0, endAngle: 0},
-                        {startAngle: 0, endAngle: 2 * Math.PI}
-                    );
-                return function (t) {
-                    return arc(interpolate(t));
-                };
-            })
-            .each("end", function (d) {
-                if (complete(g))
-                    ripples(d);
-                g.remove();
-            });
+        .attr("class", "arc")
+          .transition().duration(2000).ease('linear')
+        .attrTween("d", function (d) {
+          var interpolate = d3.interpolate(
+              {startAngle: 0, endAngle: 0},
+              {startAngle: 0, endAngle: 2 * Math.PI}
+            );
+          return function (t) {
+            return arc(interpolate(t));
+          };
+        })
+        .each("end", function (d) {
+          if (complete(g))
+            ripples(d);
+            //g.remove();
+          });
 
-    g.exit().remove().each(function () {
-        this.__stopped__ = true;
-    });
-}
+      g.exit().remove().each(function () {
+          this.__stopped__ = true;
+      });
+    }
 
-  function complete(g) {
-      return g.node().__stopped__ != true;
-  }
+    var complete = function(g) {
+      return g.node().__stopped__ !== true;
+    }
 
-  function ripples(position) {
+    var ripples = function(position) {
       for (var i = 1; i < 5; ++i) {
-          var circle = svg.append("circle")
-                  .attr("cx", position[0])
-                  .attr("cy", position[1])
-                  .attr("r", initR - (thickness / 2))
-                  .style("stroke-width", thickness / (i))
-              .transition()
-                  .delay(Math.pow(i, 2.5) * 50)
-                  .duration(2000).ease('quad-in')
-                  .attr("r", r)
-                  .style("stroke-opacity", 0)
-                  .each("end", function () {
-                      d3.select(this).remove();
-                  });
+        var circle = svg.append("circle")
+          .attr("cx", position[0])
+          .attr("cy", position[1])
+          .attr("r", initR - (thickness / 2))
+          .style("stroke-width", thickness / (i))
+        .transition()
+          .delay(Math.pow(i, 2.5) * 50)
+          .duration(2000).ease('quad-in')
+          .attr("r", r)
+          .style("stroke-opacity", 0)
+          .each("end", function () {
+              d3.select(this).remove();
+          });
       }
-  }
-};
+    }
+
+    var svg = d3.select(element[0])
+      .append("svg");
+
+    svg.on("mousedown", click)
+      .on("mouseup", click);
+
+  };
 
   return {
     restrict: 'E',
-    link: link
+    link: link,
+    scope: {selectedPoint: '='}
 
   }
 });
