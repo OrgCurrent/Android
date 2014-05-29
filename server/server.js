d95dfe242394cd8bf400891743ea9a7fb8d8dd92
Andrew Jones <acjones617@gmail.com>
2014-05-29 02:02:22 -0700

    Adds data point endpoints for testing

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 var express = require('express'),
-    path = require('path'),
-    http = require('http');
-    // data = require('./routes.js');
+	path = require('path'),
+	http = require('http');
+	// data = require('./routes.js');
 
 var app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 4000);
-    app.use(express.compress());
-    app.use(express.logger('tiny')); /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, '../www')));
+	app.set('port', process.env.PORT || 4000);
+	app.use(express.compress());
+	app.use(express.logger('tiny')); /* 'default', 'short', 'tiny', 'dev' */
+	app.use(express.bodyParser()),
+	app.use(express.static(path.join(__dirname, '../www')));
 });
 
 // app.get('/user/add/:username/:domain', data.addUser);
@@ -21,10 +21,21 @@ app.get('/', function(req, res) {
 });
 
 app.post('/user/add/:username/:domain', function(req, res) {
-    console.log(req.params.username, req.params.domain);
-    res.send({success: true});
-})
+  console.log(req.params.username, req.params.domain);
+  res.send({success: true});
+});
+
+app.post('/user/score/:username/:domain', function(req, res) {
+  console.log(req.params.username, req.params.domain);
+  res.send({success: true});
+});
+
+app.get('/domain/scores/:domain', function(req, res) {
+  console.log(req.params.domain);
+  res.send({points: [[200, 200]]});
+});
+
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
