2481d85a6700b448eec80225ba0938e36fecfdde
Andrew Jones <acjones617@gmail.com>
2014-05-29 02:04:07 -0700

    Adds function to make http post and gets for data points

--- www/js/http.js
+++ www/js/http.js
@@ -15,6 +15,26 @@ angular.module('services', [])
         data: JSON.stringify({email: username + '@' + domain}),
         headers: {'Content-Type': 'application/json'}
       })
+    },
+    sendScore: function(username, domain, score) {
+      var data = {
+        email: username + '@' + domain,
+        score: score
+      };
+      return $http({
+        method: 'POST',
+        url: 'user/score/' + username + '/' + domain,
+        data: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+      })
+    },
+    getScores: function(domain) {
+      return $http({
+        method: 'GET',
+        url: 'domain/scores/' + domain,
+      })
     }
+
+
   }
 })
