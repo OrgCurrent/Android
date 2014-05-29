76641445d523e4fb03c23e041ced333d77298673
Andrew Jones <acjones617@gmail.com>
2014-05-29 12:39:36 -0700

    Fills out random dummy data generator

--- server/server.js
+++ server/server.js
@@ -3,6 +3,45 @@ var express = require('express'),
 	http = require('http');
 	// data = require('./routes.js');
 
+var deepCopy = function(nest) {
+  var result = [];
+  for (var i = 0; i < nest.length; i++) {
+    result.push(nest[i].slice());
+  }
+  return result;
+}
+
+var pointsGenerator = function(num, rounds) {
+  var points = [];
+  thisRound = [];
+  var i = 0;
+
+  while (i < num) {
+    thisRound.push([
+      Math.random() * 9 + 0.5,
+      Math.random() * 9 + 0.5,
+      ]);
+    i++;
+  }
+
+  points.push(thisRound);
+
+  // make a copy of this round
+  var nextRound = deepCopy(thisRound);
+
+  for (var j = 0; j < rounds - 1; j++) {
+    for (var k = 0; k < thisRound.length; k++) {
+      for (var z = 0; z < 2; z++) {
+        nextRound[k][z] += 4 * Math.random() - 2;
+        nextRound[k][z] = Math.min(Math.max(nextRound[k][z], 0.5), 9.5);
+      }
+    }
+    points.push(nextRound);
+    nextRound = deepCopy(nextRound);
+  }
+  return points;
+};
+
 var app = express();
 
 app.configure(function () {
@@ -31,10 +70,7 @@ app.post('/user/score/:username/:domain', function(req, res) {
 });
 
 app.get('/domain/scores/:domain', function(req, res) {
-  var points = [
-  [[100, 50], [300,150]],
-  [[100, 150], [100, 150]]
-  ]
+  var points = pointsGenerator(10,10);
   console.log(req.params.domain);
   res.send({points: points});
 });
