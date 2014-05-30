184eff018db7231f661f8faf96ad5636c364efdc
shall <hall.steven.g@gmail.com>
2014-05-30 14:34:40 -0700

    missing semicolon

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 app.configure(function () {
   app.set('port', process.env.PORT || 4000);
   app.use(express.compress());
-  app.use(cors())
+  app.use(cors());
   app.use(express.logger('tiny')); // 'default', 'short', 'tiny', 'dev'
   app.use(express.bodyParser()),
   app.use(express.static(path.join(__dirname, '../client')));
