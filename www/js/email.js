805e8464ab56b0cd1cc806f64a7df39dede04f5f
Andrew Jones <acjones617@gmail.com>
2014-05-30 22:32:13 -0700

    Updates logic for email input after seeing server logic

--- www/js/email.js
+++ www/js/email.js
@@ -2,7 +2,7 @@ angular.module('app.email', [
   'services'
   ])
 
-.controller('EmailCtrl', function($scope, $http, HttpFactory, $state){
+.controller('EmailCtrl', function($scope, $rootScope, $http, HttpFactory, $state){
   console.log('email');
   $scope.$emit('email');
   $scope.submitted = false;
@@ -10,50 +10,74 @@ angular.module('app.email', [
 
   $scope.sendEmail = function() {
 
-    // HttpFactory.jq();
-
-    // if (false) {
     if($scope.email_form.$valid) {
       
       var username = $scope.email.userInput.split('@')[0]
       var domain = $scope.email.userInput.split('@')[1]
 
-      console.log('sent ', $scope.email.userInput); // 'keith@email.com'
+      console.log('sent ', $scope.email.userInput);
       HttpFactory.sendEmail(username, domain)
        .success(function(data, status, headers, config) { 
-          // called when user already in system.
+          // if user already in system, this will show status === 'existing'
+          // otherwise, status === 'new'
           console.log('success!');
           console.log('data: ', data 
                     + '\nstatus: ', status
                     + '\nheaders:', headers
                     + '\nconfig:', config);
           if (data.status === 'new') {
-            $state.go('home.verify');
+            HttpFactory.postUser(username, domain)
+              .success(function() {
+                console.log($rootScope.domain);
+                $rootScope.username = username;
+                $rootScope.domain = domain;
+                $state.go('home.verify');
+              })
+
           } else if (data.status === 'existing') {
+            // if existing, check if verified yet
+            console.log('existing');
             HttpFactory.verify(username, domain)
-              .success(function(data) { 
-                if (data.status === true) {
-                  $state.go('home.opinion');
-                } else {
-                  HttpFactory.resendEmail(username,domain)
+              .success(function(verifyData) {
+                if (verifyData.status === false) {
+                  // user exists, but not verified yet - send along to verify page
+                  HttpFactory.postUser(username, domain)
                     .success(function() {
+                      $rootScope.username = username;
+                      $rootScope.domain = domain;
                       $state.go('home.verify');
-                    })
-                    .error(function(data) {
-                      console.log('error', data);
-                    })
+                    });
+                } else if (verifyData.status === true) {
+                  // show error message - verified email already exists - MIGHT WANT TO UPDATE LOGIC IN FUTURE
+                  console.log('user exists');
+                  $scope.notUnique = true;
                 }
               })
+
+            // HttpFactory.verify(username, domain)
+            //   .success(function(data) { 
+            //     if (data.status === true) {
+            //       $state.go('home.opinion');
+            //     } else {
+            //       HttpFactory.resendEmail(username,domain)
+            //         .success(function() {
+            //           $state.go('home.verify');
+            //         })
+            //         .error(function(data) {
+            //           console.log('error', data);
+            //         })
+            //     }
+            //  })
           }
       })
       .error(function(data, status, headers, config) {
-          // this is called when new user entered
+          // server error
           console.log('error!')
           console.log('data: ', data 
                     + '\nstatus: ', status
                     + '\nheaders', headers
                     + '\nconfig', config)
-          $state.go('home.verify');
+          $scope.serverError = true;
       });
     } else {
       $scope.email_form.submitted = true;
