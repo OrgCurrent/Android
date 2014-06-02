871c7f42e434a127a837e3490e3911621db2b2e4
Andrew Jones <acjones617@gmail.com>
2014-06-02 11:47:40 -0700

    Adds a third graphic to show individual points

--- www/js/graphGraphics.js
+++ www/js/graphGraphics.js
@@ -318,47 +318,17 @@ angular.module('graphics', [])
 .factory('PointGraph', ['$rootScope', 
   function($rootScope) {
   return {
-    dailyAvg: function(data, margin) {
+    animate: function(data, margin) {
       var recentScores = [];
+      var status = 0;
+
       for (var i = 0; i < data.length; i++) {
         var numScores = data[i].scores.length;
-        var userScores = [];
-        for (var j = 3; j > 0; j--) {
-          if (j > numScores) {
-            userScores.push([]);
-          } else {
-            userScores.push([data[i].scores[numScores - j].x,data[i].scores[numScores - j].y]);
-          }
-        }
-        recentScores.push(userScores);
+          if (numScores && data[i].username !== window.localStorage.username) {
+            recentScores.push([data[i].scores[numScores - 1].x, data[i].scores[numScores - 1].y]);
+          } else console.log(numScores);
       }
-      // recentScores now contains x arrays (one per user) of most 3 recent scores.
-
-      var avgScores = [[50,50]];
-      for (var i = 0; i < 3; i++) {
-        var totalX = 0;
-        var totalY = 0;
-        var num = 0;
-        for (var j = 0; j < recentScores.length; j++) {
-          // console.log(recentScores[j][0])
-          if (recentScores[j][i][0] !== undefined) {
-            totalX += recentScores[j][i][0];
-            totalY += recentScores[j][i][1];
-            num += 1;
-          }
-        }
-        if (num > 0) {
-          avgScores.push([totalX/num, totalY/num]);
-        }
-      }
-      // avgScores now contains avg score of most 3 recent scores.
-      // TEMP FOR TESTING
-      avgScores = [[50,50],[20,30],[70,90],[40,5]]; 
-      // TEMP FOR TESTING
-
-      var dataPoints = avgScores.length;
-      var status = 0;
-      var lastRound = false;
+      // recentScores now contains an array of most recent score for each user.
 
       var svg = d3.select('svg');
       var width = svg[0][0].clientWidth - margin.left;
@@ -379,36 +349,15 @@ angular.module('graphics', [])
         return (yMin - y/100 * (yMin - yMax));
       }
 
-      var animate = function(data) {
+      var dataPoints = recentScores.length;
+
+      var progressBarUpdate = function() {
         status += (100 * 1./dataPoints);
         progressBar(status);
-
-        if (!lastRound) {
-          var thisRoundData = [data.shift()];
-        } else {
-          var thisRoundData = '';
-        }
-
-        path(thisRoundData);
-        movingAvg(thisRoundData);
-
-
-        if (!lastRound) {
-          setTimeout(function() {
-            // status += (100 * 1./totalDays);
-            // $rootScope.$broadcast('status', status);
-            if (data.length > 0) {
-              animate(data);
-            } else {
-              lastRound = true;
-              animate();
-            }
-          }, 1100);
-        }
       };
 
       var initR = 10;
-      var r = 100;
+      var r = 50;
       var thickness = 3;
 
       var ripples = function(position) {
@@ -430,24 +379,11 @@ angular.module('graphics', [])
         
       };
 
-      var movingAvg = function(data) {
+      var placePoints = function(data) {
         // circle
         // data join
         var others = svg.selectAll("circle.others")
           .data(data)
-        
-        // update
-        others.transition().duration(1000)
-          .tween('ripple', function(d, i) {
-            console.log('tween', d, i, this);
-            return function() {
-              ripples([this.cx.animVal.value, this.cy.animVal.value]);
-            }.bind(this)
-          })
-          .attr({
-            cx: function(d) { return translateX(d[0]); },
-            cy: function(d) { return translateY(d[1]); }
-          });
 
         // enter
         others.enter()
@@ -457,60 +393,60 @@ angular.module('graphics', [])
             cx: function(d) { return translateX(d[0]); },
             cy: function(d) { return translateY(d[1]); },
             r: 0,
-            fill: 'black'
+            // fill: 'black'
           })
-            .transition()
-          .attr('r', 5);
-
-        // exit
-        others.exit()
-            .transition().duration(1000)
-          .attr({r: 15})
-            .transition().duration(1000)
-          .attr({r: 5})
-            .transition().duration(1000)
-          .attr({r: 15})
-            .transition().duration(1000)
-          .attr({r: 5})
-            .transition().duration(1000)
-          .attr({r: 10})
-      }
-
-
-      var path = function(data) {
-        // path
-        // data join
-        var trail = svg.selectAll(".trail")
-          .data(data)
-        
-        // update
-        trail
-          .attr({
-            d: function(d) {return trail.attr('d') + ' L ' + translateX(d[0]) + ' ' + translateY(d[1]); },
+            .transition().delay(function(d, i) { return 50 * i }).duration(200)
+          .attr('r', 5)
+          .each('end', function() {
+            progressBarUpdate();
+            ripples([this.cx.animVal.value, this.cy.animVal.value]);
           });
-
-        // enter
-        trail.enter()
-          .append('path')
-          .attr({
-            class: 'trail',
-            'stroke-dasharray': '2,4,2',
-            d: function(d) {return 'M ' + translateX(d[0]) + ' ' + translateY(d[1]); },
-          })
       }
+
+      var line = d3.svg.line()
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; });
+
+      // Derive a linear regression
+      var lin = ss.linear_regression().data(recentScores.map(function(d) {
+        return [translateX(d[0]), translateY(d[1])];
+      })).line();
+
+      // Create a line based on the beginning and endpoints of the range
+      var linData =  recentScores.map(function(d) {
+        return {
+          x: translateX(d[0]),
+          y: lin(translateX(d[0]))
+        };
+      });
+
+
+      var path = function() {
+
+        svg.append('path')
+            .datum(linData)
+            .attr({
+              'stroke-dasharray': '2,4,2',
+              'd': line,
+              fill: 'none',
+              stroke: 'black',
+              'stroke-width': 3
+            })
+      };
 
 
       var progressBar = function(status) {
         // PROGRESS BAR
         // data join
-        if (status === 100) {
+        if (status > 95) {
           $rootScope.$broadcast('complete');
+          setTimeout(path, 1000);
         }
         var progress = d3.select('.graph').selectAll('progress')
           .data([status]);
 
         // update 
-        progress.transition().ease('linear').duration(1000)
+        progress.transition().ease('linear').duration(30)
           .attr({
             value: function(d) { return d; }
           });
@@ -523,9 +459,9 @@ angular.module('graphics', [])
             value: function(d) { return d; }
           })
       };
-
-      progressBar(status);
-      animate(avgScores);
+      placePoints(recentScores);
+      // progressBar(status);
+      // animate(avgScores);
     }
   }
 }]);
