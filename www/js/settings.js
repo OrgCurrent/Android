28f64971dd78ba60568e8ebdbbf4a95a237b865b
Andrew Jones <acjones617@gmail.com>
2014-06-03 17:57:26 -0700

    Creates directive and html template for settings side-view

---
+++ www/js/settings.js
@@ -0,0 +1,23 @@
+angular.module('app.home.settings', 
+  [])
+
+.directive('settings', function() {
+  return {
+    restrict: 'EA',
+    templateUrl: 'templates/settings.html',
+    scope: true,
+    controller: ['$scope', '$rootScope', '$state', '$ionicSideMenuDelegate',
+      function($scope, $rootScope, $state, $ionicSideMenuDelegate) {
+        console.log('settings');
+        var local = window.localStorage;
+
+        $scope.changeEmail = function() {
+          local.clear();
+          $rootScope.$broadcast('resetStorage');
+          $ionicSideMenuDelegate.toggleLeft();
+          $state.go('home.email');
+        }
+    }]
+  }
+  
+})
